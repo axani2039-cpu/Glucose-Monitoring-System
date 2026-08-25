984ab87832cd610343593588a23e4452c112849a
lili2005 <84999154+lili2005@users.noreply.github.com>
2026-08-25 16:37:44 +0300

    EXTI INTER/PRI/CFG

---
+++ INCLUDE/MCAL/EXTI/EXTI_CFG.h
@@ -0,0 +1,13 @@
+/*
+ * EXTI_CFG.h
+ *
+ *  Created on: Aug 25, 2026
+ *      Author: ahmed
+ */
+
+#ifndef INCLUDE_MCAL_EXTI_EXTI_CFG_H_
+#define INCLUDE_MCAL_EXTI_EXTI_CFG_H_
+
+
+
+#endif /* INCLUDE_MCAL_EXTI_EXTI_CFG_H_ */
